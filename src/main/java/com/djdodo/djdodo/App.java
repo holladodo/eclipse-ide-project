2947c2f3fce5b42e71e7f257d4082417c0bbddcc
Olu Bademosi <holladodo@gmail.com>
2023-11-26 23:20:56 -0500

    Eclipse Project

---
+++ src/main/java/com/djdodo/djdodo/App.java
@@ -0,0 +1,13 @@
+package com.djdodo.djdodo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
